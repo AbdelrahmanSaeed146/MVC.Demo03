4a48cec7f0793e973752de83663b0bbc8d00c4bc
John Doe <saeedabdelrahman48@gmail.com>
2024-03-20 04:41:20 +0200

    Part 04 GitHub - Assignment  (Continued)

---
+++ MVC.Demo03.DAL/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Demo03.DAL.Data.configurations;
+using MVC.Demo03.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Demo03.DAL.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       => optionsBuilder.UseSqlServer("Server = . ; Datebase = MvcApp;Trusted_Connection=True");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());  
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ MVC.Demo03.DAL/Data/configurations/DepartmentConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC.Demo03.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Demo03.DAL.Data.configurations
+{
+    internal class DepartmentConfigurations :IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+           builder.Property(D=>D.Id).UseIdentityColumn(10,10);
+           builder.Property(D=>D.Code).IsRequired().HasColumnType("varchar").HasMaxLength(50);
+           builder.Property(D=>D.Name).IsRequired().HasColumnType("varchar").HasMaxLength(50);
+        }
+
+      
+    }
+}
