79c6fb8c2e0ffd4ee10926b23927069f217ede1b
John Doe <saeedabdelrahman48@gmail.com>
2024-04-03 02:12:34 +0200

    Part 01 Document Setting

---
+++ MVC.Demo03.PL/Helpers/DocumentSetting.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace MVC.Demo03.PL.Helpers
+{
+    public static class DocumentSetting
+    {
+        public static string UploadFile( IFormFile File, string FolderName)
+        {
+            //string FolderPath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\Files\\{FolderName}";
+
+            string FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName);
+
+            if (Directory.Exists(FolderPath))
+                Directory.CreateDirectory(FolderPath);
+            
+            string FileName = $"{Guid.NewGuid()}{Path.GetExtension(File.FileName)}";
+
+            string FilePath = Path.Combine(FolderPath, FileName);
+
+            using var FileStream = new FileStream(FilePath,FileMode.Create);
+
+            File.CopyTo(FileStream);
+
+            return FileName;
+        }
+
+        public static void DeleteFile(string FileName , string FolderName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName);
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+        }
+    }
+}
